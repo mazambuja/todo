3653d1430d6a186fd202955cf2ef2577ae269365
Marcelo Azambuja <marcelo@Marcelos-MacBook-Pro.local>
2014-08-28 17:50:01 +0300

    Added a table to the first screen and renamed the labels of the first and second view.

---
+++ TaskManager.swift
@@ -0,0 +1,13 @@
+//
+//  TaskManager.swift
+//  Todo
+//
+//  Created by Marcelo Azambuja on 8/28/14.
+//  Copyright (c) 2014 MGA Software. All rights reserved.
+//
+
+import Cocoa
+
+class TaskManager: NSObject {
+
+}
---
+++ Todo/TaskManager.swift
@@ -0,0 +1,25 @@
+//
+//  TaskManager.swift
+//  Todo
+//
+//  Created by Marcelo Azambuja on 8/28/14.
+//  Copyright (c) 2014 MGA Software. All rights reserved.
+//
+
+import Foundation
+
+var taskMgr = TaskManager()
+
+struct task{
+    var name = "Un-Named"
+    var desc = "Un-Described"
+}
+
+class TaskManager
+{
+    var tasks = [task]()
+    
+    func addTask(name:String, desc:String){
+        tasks.append(task(name: name, desc: desc))
+    }
+}
